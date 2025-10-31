de7a2889b763b3580118ed91b131d363318fcdb1
d4nin <daninsadzak@gmail.com>
2025-10-31 23:13:07 +0100

    Variable, unos i izrazi - Zadatak 4

---
+++ 1. Varijable, unos i izrazi/Zadatak_4.py
@@ -0,0 +1,9 @@
+# Napisati program koji pretvara temperaturu iskazanu u stepenima Celzijusa u temperaturu iskazanu u stepenima
+# Fahrenheita. Odnos izmedju dvije temperaturne skale je ... gdje je C temperatura iskazana u stepenima 
+# Celzijusa a F temperatura iskazana u stepenima Fahrenheita. 
+
+C = float(input())
+
+F = C * (9 / 5) + 32 
+
+print(F)
